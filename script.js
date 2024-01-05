154e451e1b9a60b844092d1b1b582aff7af54a4a
u-dani <daniellebezerra.dev@gmail.com>
2024-01-04 22:46:56 -0300

    password button changes input type

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const inputLogin = document.querySelector('#input-login')
-const btnCancel = document.querySelector('.js-btn-cancel')
+const inputPassword = document.querySelector('#input-password');
+const btnCancel = document.querySelector('.js-btn-cancel');
+const btnTogglePassword = document.querySelector('.js-icon-password');
 
-btnCancel.addEventListener('click', ({ target }) => target.previousElementSibling.value = "")
+btnCancel.addEventListener('click', ({ target }) => target.previousElementSibling.value = "");
+btnTogglePassword.addEventListener('click', () => {
+    inputPassword.getAttribute("type") === "text"
+        ? inputPassword.setAttribute("type", "password")
+        : inputPassword.setAttribute("type", "text");
+});
