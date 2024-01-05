e19db1a68e7bbf6c57d839231560b4d58e3a57c8
u-dani <daniellebezerra.dev@gmail.com>
2024-01-05 15:25:20 -0300

    togglePasswordVisibility function

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+const visibilityIconPath = 'images/icon-visibility.svg';
+const visibilityOffIconPath = 'images/icon-visibility-off.svg';
+
 const inputPassword = document.querySelector('#input-password');
 const btnCancel = document.querySelector('.js-btn-cancel');
 const btnTogglePassword = document.querySelector('.js-icon-password');
 
+function togglePasswordVisibility({ target }) {
+    const input = target.previousElementSibling;
+    
+    if (input.getAttribute("type") === "text") {
+        input.setAttribute("type", "password");
+        target.setAttribute("src", visibilityIconPath);
+    }
+    else {
+        inputPassword.setAttribute("type", "text");
+        target.setAttribute("src", visibilityOffIconPath);
+    }
+}
+
 btnCancel.addEventListener('click', ({ target }) => target.previousElementSibling.value = "");
-btnTogglePassword.addEventListener('click', () => {
-    inputPassword.getAttribute("type") === "text"
-        ? inputPassword.setAttribute("type", "password")
-        : inputPassword.setAttribute("type", "text");
-});
+btnTogglePassword.addEventListener('click', togglePasswordVisibility);
