fd2b2c64c5745bbb2ef41f632d6c5d0036b5713f
u-dani <daniellebezerra.dev@gmail.com>
2024-01-09 12:15:41 -0300

    change password message

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ const visibilityOffIconPath = 'images/icon-visibility-off.svg';
 const allCancelButtons = document.querySelectorAll('.js-btn-cancel');
 const passwordInput = document.querySelector('#input-password');
 const passwordIcon = document.querySelector('#input-password ~ button img');
+const passwordMessage = document.querySelector('#password-message');
 const btnTogglePassword = document.querySelector('.js-btn-password');
 
 function togglePasswordVisibility() {
     if (passwordInput.getAttribute("type") === "text") {
         passwordInput.setAttribute("type", "password");
         passwordIcon.setAttribute("src", visibilityIconPath);
+        passwordMessage.textContent = 'show password';
     }
     else {
         passwordInput.setAttribute("type", "text");
         passwordIcon.setAttribute("src", visibilityOffIconPath);
+        passwordMessage.textContent = 'hide password';
     }
 }
 
